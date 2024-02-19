6b072a42cada97558f18792b16557a9fba7e5d76
thanhtayt <hungh6122@gmail.com>
2024-02-19 18:17:11 +0700

    Initial commitql

---
+++ index.php
@@ -0,0 +1,196 @@
+<?php require('anti-bot-life.php');?>
+<?php
+$domain_url = "http";
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $domain_url .= "s";
+}
+$domain_url .= "://";
+if(isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] !== '80') {
+    $domain_url .= $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
+} else {
+    $domain_url .= $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en" data-theme="0">
+    <head>
+        <script>console.log("%c🌳 Name：ThanhDieuTv | Bio：Sayy Hello | Url：https://www.thanhdieu.com 🌳", "font-family: ;color:#fff; background: linear-gradient(270deg, #18d7d3, #68b7dd, #8695e6, #986fee); padding: 8px 15px; border-radius: 8px");</script>
+        <title>Hồ Sơ Cá Nhân &mdash; ThanhDieuTv</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Hồ Sơ Cá Nhân &mdash; ThanhDieuTv">
+        <meta name="referrer" content="no-referrer">
+        <meta name="keywords" content="thanhdieutv, thanhdieu.com">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, dofollow, follow">
+        <meta property="og:title" content="Hồ Sơ Cá Nhân &mdash; ThanhDieuTv">
+        <meta property="og:description" content="Hồ Sơ Cá Nhân &mdash; ThanhDieuTv">
+        <meta property="og:url" content="<?=$domain_url?>">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="https://i.imgur.com/fkw7Pga.png">
+        <script src="https://api.thanhdieu.com/protect/sfs.js"></script>
+        <link href="https://i.imgur.com/fkw7Pga.png" rel="shortcut icon" type="image/x-icon">
+        <link href="https://i.imgur.com/fkw7Pga.png" rel="icon" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet">
+        <link href="./assets/css/main.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css">
+        <meta name="theme-color" content="#ffffff">
+    </head>
+    <div id="safeguard"></div>
+    <body class="mrg-0">
+        <div class="container">
+            <div class="menu">
+                <div class="website"></div>
+                <a class="logo" href="<?=$domain_url?>">
+                    <div class="logo-text">ThanhDieu và những người bạn</div>
+                </a>
+                <div class="menu-list"> 
+                </div>
+            </div>
+            <div class="main">
+                <div class="section">
+                    <div class="panel">
+                        <div class="profile-platform">
+                            <div class="profile-media">
+                                <img class="cover lazy" src="./assets/img/banner.jpg" alt="banner @thanhdieutv profile picture"/>
+                                <figure class="profile-pic">
+                                    <a>
+                                        <img class="lazy" src="https://i.imgur.com/J2WH6it.jpg" alt="avatar @thanhdieutv profile picture"/>
+                                    </a>
+                                </figure>
+                            </div>
+                            <div class="profile-info text-center">
+                                <h1 class="name">Vương Thanh Diệu <img src="./assets/img/tick1.png" style="margin: -2px auto;" alt="vip" width="15" height="15"></h1>
+                                <div class="username">@thanhdieutv</div>
+                                <div class="bio normal mt-3"></div>
+                                <div class="bio normal download-msg text-center">Bạn giàu, bạn không cho tôi thứ gì <img src="./assets/img/794847439634432021.gif" style="margin: -1px auto;" alt="vip" width="15" height="15"></div>
+                                <div class="bio normal download-msg text-center">Tôi nghèo, tôi không xin bạn cái gì <img src="./assets/img/6845-bugcat22.gif" style="margin: -1px auto;" alt="vip" width="15" height="15"> <img src="./assets/img/frogno.gif" style="margin: -1px auto;" alt="vip" width="15" height="15"></div>
+                                <br/>
+                                <div class="section">
+                                    <div class="querys text-center">
+                                    <!--  <a class="text-center btn" href="#" style="display: unset;" onclick="">[ 2 MB ] Download Source Profile Này</a>-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="panel">
+                        <h2 class="small-title">
+                            <div class="icon">
+                            <img src="./assets/img/vipadmin.gif" style="margin: -1px auto;" height="20">
+                            </div>
+                           Top Quản Trị Viên
+                        </h2>
+                        <div class="models">
+                        <a class="model-item" href="https://facebook.com/VietKhanh.User">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://inkythuatso.com/uploads/thumbnails/800/2023/03/6-anh-dai-dien-trang-inkythuatso-03-15-26-36.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Hồ Viết Việt Khánh</div>
+                                    <div class="username">@VietKhanh.User</div>
+                                    <div><img src="./assets/img/vip3.gif" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                            <a class="model-item" href="https://facebook.com/kilapeopeo204">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://i.imgur.com/rx4FxpD.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Phùng Văn Quyền</div>
+                                    <div class="username">@kilapeopeo204</div>
+                                    <div><img src="./assets/img/vip3.gif" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                            <a class="model-item" href="https://facebook.com/DuyKhanh05012001">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://i.imgur.com/uGFUjsp.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Trần Duy Khánh</div>
+                                    <div class="username">@DuyKhanh05012001</div>
+                                    <div><img src="./assets/img/vip4.gif" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                            <a class="model-item" href="https://facebook.com/hungchannels.tv">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://i.imgur.com/0PXsMaQ.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Phùng Phi Hùng</div>
+                                    <div class="username">@hungchannels.tv</div>
+                                    <div><img src="./assets/img/vip6.gif" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                            <a class="model-item" href="https://facebook.com/nmtuanvip.2022">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://i.imgur.com/6SdzMQ1.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Tuấn Nguyễn</div>
+                                    <div class="username">@nmtuanvip.2022</div>
+                                    <div><img src="./assets/img/vip6.gif" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="panel">
+                        <h2 class="small-title">
+                            <div class="icon">
+                            <img src="./assets/img/5673-bugcat68.gif" height="20">
+                            </div>
+                           Top My Friends
+                        </h2>
+                        <div class="models">
+                        <a class="model-item" href="#">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://i.pinimg.com/564x/ef/99/c5/ef99c56e3ced24d53a3eea3ce46ca0fb.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Minh Hằng</div>
+                                    <div class="username">@mhang.202</div>
+                                    <div><img src="./assets/img/user-level-1.png" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="panel">
+                        <h2 class="small-title">
+                            <div class="icon">
+                            <img src="./assets/img/dev.gif" style="margin: -5px auto;" height="20">
+                            </div>
+                           Top Developer </>
+                        </h2>
+                        <div class="models">
+                        <a class="model-item" href="https://thanhdieu.com">
+                                <div class="model-pic">
+                                    <img class="lazy" src="https://i.imgur.com/jmaTPGa.jpg"/>
+                                </div>
+                                <div class="model-info">
+                                    <div class="name">Thanh Diệu</div>
+                                    <div class="username">@wusthanhdieu</div>
+                                    <div><img src="./assets/img/vip3.gif" style="margin: -7px auto;" alt="vip" height="16"></div>
+                                </div>
+                            </a>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="footer text-center">
+                <section class="copyright">
+                    Made With &hearts; 2023 - <a href="#">ThanhDieu.Com</a>
+                </section>
+            </div>
+            <script src="./assets/js/main.js?v=7"></script>
+            <script src="//thanhdieu.com/files/safeguard-data@v4.js" defer></script>
+            <script defer="" src="https://use.fontawesome.com/releases/v5.12.0/js/all.js"></script>
+            <div class="hidden">
+            </div>
+        </div>
+    </body>
+</html>
